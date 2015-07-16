bdbd496eca5d325f7ebf8c1f4cbb67f14011f0ad
root <root@markit-4a99271e.fcsinternal.com>
2015-07-16 16:58:26 -0600

    more stuff

--- src/com/ravenpack/feed/RavenPackFeed.java
+++ src/com/ravenpack/feed/RavenPackFeed.java
@@ -33,10 +33,71 @@ public class RavenPackFeed
 	private long fileStartDate = 0;
 	private long fileEndDate = 0;
 	
+	//Retry a TimeSeriesRange request this number of times
+	public int rangeAttempts = 1;
 	
+	//Use the default package-pribate access level for this variable.
+	//It controls whether or not we switch files when we get to a new month
+	boolean switchFilenames = true;
 	
-	public static void main(String args[]){
-		System.out.println("Hello World");
+	public RavenPackFeed ( DataFeedTool tool, DataGatewayClient dg, Calendar cal, 
+			LinkedHashMap<String, DataFeedSymbol> symbols, int rangeAttempts){
+		{
+			this.dg = dg;
+			this.cal = cal;
+			this.parent = tool;
+			this.symbols = symbols;
+			this.rangeAttempts = rangeAttempts;
+		}
+		
+		
+		private void updateFileDates( long refdate )
+		{
+			// Set the calendar with the refdate
+			cal.setTimeInMillis(refdate);
+			// Get the start of that month
+			cal.set(Calendar.MILLISECOND, 0);
+			cal.set(Calendar.SECOND, 0);
+			cal.set(Calendar.MINUTE, 0);
+			cal.set(Calendar.HOUR_OF_DAY, 0);
+			cal.set(Calendar.DAY_OF_MONTH, 1);
+			// Set the month start of this file
+			fileStartDate = cal.getTimeInMillis();
+			// Set the month end of this file
+			cal.add(Calendar.MONTH, 1);
+			fileEndDate = cal.getTimeInMillis();
+		}
+		
+		/**
+	     * <p>When we're appending a TimeSeriesRange to an existing file,
+	     *    we must request the data including the last entry because timestamps
+	     *    are not unique in the data.   When writing back out, we want to make
+	     *    sure we start writing after the last known id.</p>
+	     */
+		private int findStartId( TimeSeriesRange range, String last_id )
+		{
+			// Index from -1 so that if we don't find anthing, the next line will
+			// be zero
+			int index = -1;
+			if( range.size() > 0) {
+				//We only need to search the first second.
+				long  firstSec = (long)Math.floor(range.getTimeAt(0)/1000);
+				for( int i = 0; i < range.size(); i++) {
+					// Once we've gone past the first second we're done.
+					if( Math.floor(range.getTimeAt(i)/1000) > firstSec ) {
+						break;
+					}
+					// If we found our id then save the index
+					// but continue since itcould occur multiple times
+					String current_id = 
+							range.getPropertyAt(i, DataFeedSymbol.storyIdField);
+					if( last_id.equals(current_id)) {
+						index = i;
+					}
+				}
+			}
+			return index;
+		}
 	}
 
 }
